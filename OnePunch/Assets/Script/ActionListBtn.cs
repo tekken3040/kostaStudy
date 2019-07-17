1762226f2de535f980d6102de8cfb969c92876ed
Limeparfait <tekken3040@naver.com>
2019-07-17 11:29:48 +0900

    33333

---
+++ OnePunch/Assets/Script/ActionListBtn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionListBtn : MonoBehaviour
+{
+    [SerializeField] GameObject Edge;
+
+    public void OnEdgeActive(bool isActive)
+    {
+        Edge.SetActive(isActive);
+    }
+}
---
+++ OnePunch/Assets/Script/TopPanelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopPanelManager : MonoBehaviour
+{
+    [SerializeField] Image imgPlayerHP, imgEnemyHP;
+    [SerializeField] Image imgPlayerWins, imgEnemyWins;
+    [SerializeField] Text txtRoundCnt;
+
+    float fPlayerHP = 1f;
+    float fEnemyHP = 1f;
+    float parameterHP = 0.01f;
+
+    public enum TARGET
+    {
+        NONE = 0,
+        PLAYER,
+        ENEMY,
+    }
+
+    public void SetHPImage(TARGET target, float fHP)
+    {
+        switch(target)
+        {
+            case TARGET.PLAYER:
+                imgPlayerHP.fillAmount -= fHP * parameterHP;
+                break;
+
+            case TARGET.ENEMY:
+                break;
+        }
+    }
+}
