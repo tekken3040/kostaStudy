036ce85b0864df8505fad8bd73a86803a1f6c83d
Limeparfait <tekken3040@naver.com>
2019-08-07 17:40:24 +0900

    190807 작업

---
+++ Minimap/Assets/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       MapObject[] mapObjects= GameObject.FindObjectsOfType<MapObject>() ;
+        print("mapObjects.Length" +mapObjects.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
