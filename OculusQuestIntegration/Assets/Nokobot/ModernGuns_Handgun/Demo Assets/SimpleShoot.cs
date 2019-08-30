f758c0491257c4599c47b11410ada8fa4ae4adaa
Limeparfait <tekken3040@naver.com>
2019-08-30 17:44:24 +0900

    190830

--- OculusQuestIntegration/Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
+++ OculusQuestIntegration/Assets/Nokobot/ModernGuns_Handgun/Demo Assets/SimpleShoot.cs
@@ -1,62 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SimpleShoot : MonoBehaviour
 {
-
     public GameObject bulletPrefab;
     public GameObject casingPrefab;
     public GameObject muzzleFlashPrefab;
     public Transform barrelLocation;
     public Transform casingExitLocation;
-
-
+    public Text CountText;
     public float shotPower = 100f;
+
+    private int bulletCnt = 15;
 
     void Start()
     {
-        if (barrelLocation == null)
+        if(barrelLocation == null)
             barrelLocation = transform;
     }
 
-    void Update()
-    {
-        //if (Input.GetButtonDown("Fire1"))
-        /*
-        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
-        {
-            GetComponent<Animator>().SetTrigger("Fire");
-        }
-        */
-    }
     public void ShootTrigger()
     {
-        GetComponent<Animator>().SetTrigger("Fire");
+        if(bulletCnt > 0)
+            GetComponent<Animator>().SetTrigger("Fire");
     }
 
     void Shoot()
     {
-        //  GameObject bullet;
-        //  bullet = Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation);
-        // bullet.GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
-
         GameObject tempFlash;
-       Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
-       tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
-
-       // Destroy(tempFlash, 0.5f);
-        //  Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation).GetComponent<Rigidbody>().AddForce(casingExitLocation.right * 100f);
-       
+        Instantiate(bulletPrefab, barrelLocation.position, barrelLocation.rotation).GetComponent<Rigidbody>().AddForce(barrelLocation.forward * shotPower);
+        tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+        bulletCnt--;
+        CountText.text = bulletCnt.ToString();
     }
 
     void CasingRelease()
     {
-         GameObject casing;
+        GameObject casing;
         casing = Instantiate(casingPrefab, casingExitLocation.position, casingExitLocation.rotation) as GameObject;
         casing.GetComponent<Rigidbody>().AddExplosionForce(550f, (casingExitLocation.position - casingExitLocation.right * 0.3f - casingExitLocation.up * 0.6f), 1f);
         casing.GetComponent<Rigidbody>().AddTorque(new Vector3(0, Random.Range(100f, 500f), Random.Range(10f, 1000f)), ForceMode.Impulse);
     }
-
-
 }
---
+++ OculusQuestIntegration/Assets/Scripts/CustomPhotonTransformView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CustomPhotonTransformView : MonoBehaviour, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if(stream.IsWriting)
+        {
+            stream.SendNext("");
+        }
+        else
+        {
+            stream.ReceiveNext();
+        }
+    }
+}
